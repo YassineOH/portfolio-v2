9a1facb28ecedec69d3a9c646779548bb01d8131
yassine <yassine.yas98@gmail.com>
2024-08-03 19:37:08 +0100

    adding theme scripts

---
+++ src/lib/theme/index.ts
@@ -0,0 +1,28 @@
+export const getThemePreference = () => {
+  if (typeof localStorage !== undefined && localStorage.getItem('theme')) {
+    return localStorage.getItem('theme');
+  }
+  return window.matchMedia('(prefer-color-schema: dark)').matches
+    ? 'dark'
+    : 'light';
+};
+
+export const toggleTheme = () => {
+  const isDark = getThemePreference() === 'dark';
+  document.documentElement.classList[isDark ? 'add' : 'remove']('dark');
+  localStorage.setItem('theme', isDark ? 'dark' : 'light');
+};
+
+export const initializeTheme = () => {
+  toggleTheme();
+  if (typeof localStorage !== undefined) {
+    const observer = new MutationObserver(() => {
+      const isDark = getThemePreference() === 'dark';
+      localStorage.setItem('theme', isDark ? 'dark' : 'light');
+    });
+    observer.observe(document.documentElement, {
+      attributeFilter: ['class'],
+      attributes: true,
+    });
+  }
+};
---
+++ src/scripts/index.ts
@@ -0,0 +1,5 @@
+import { initializeTheme } from '~/lib/theme';
+
+window.onload = () => {
+  initializeTheme();
+};
