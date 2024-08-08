066a0b38dbc14fc0615ded5ec8a9d623bbedfce1
yassine <yassine.yas98@gmail.com>
2024-08-08 16:40:16 +0100

    tech collections added

---
+++ src/content/config.ts
@@ -0,0 +1,24 @@
+import { defineCollection, z } from 'astro:content';
+
+const techsCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+    link: z.string().url(),
+    icon: z.string(),
+    name: z.string(),
+    features: z.array(z.string()).optional(),
+    tag: z.enum([
+      'framework',
+      'styling',
+      'tool',
+      'core',
+      'language',
+      'db',
+      'env',
+      'design',
+      'state',
+    ]),
+  }),
+});
+
+export const collections = { techsCollection };
---
+++ src/lib/types.ts
@@ -0,0 +1,16 @@
+export interface LeetCodeStatus {
+  status: string;
+  message: string;
+  totalSolved: number;
+  totalQuestions: number;
+  easySolved: number;
+  totalEasy: number;
+  mediumSolved: number;
+  totalMedium: number;
+  hardSolved: number;
+  totalHard: number;
+  acceptanceRate: number;
+  ranking: number;
+  contributionPoints: number;
+  reputation: number;
+}
